0a7f162829fb8de39b66095c9fac7e9d235986ba
kanchan7c <kanchan.chaudhary77@gmail.com>
2024-02-27 17:27:44 +0530

    typecasting

---
+++ typecasting.js
@@ -0,0 +1,9 @@
+let age = '23';
+console.log(typeof age); // returns string
+
+let ageInNumber = Number(age);
+console.log(typeof ageInNumber); // returns number
+
+let weight = '67sdc';
+let weightInNumber = Number(weight);
+console.log(typeof weightInNumber);
