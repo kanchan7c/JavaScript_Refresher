98942468e17ea9689c5f95108dfb5f74e861bc78
kanchan7c <kanchan.chaudhary77@gmail.com>
2024-02-27 17:16:56 +0530

    js refresher variables and datatypes completed

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+'use strict'; //treat all JS code as newer version as per ECMA Standard
+
+// alert('hello') // available inside browser, we are using node JS here
+
+let name = 'nobody'; // String
+let age = 29; // Number
+let isTall = false; // Boolean
+// bigint -> very big number greater than 2^53
+let cat = null; // null
+let state; // undefined
+// symbol => unique
+// object
+
+console.log(typeof cat); // output is object
---
+++ variables.js
@@ -0,0 +1,19 @@
+const accountId = 123455;
+let accountEmail = 'abc@example.com';
+var accountPassword = '123442';
+accountCity = 'Gurgaon';
+let accountState;
+
+// accountId = 23 // not allowed to change const variable
+
+console.log(accountId);
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+// uninitialized variables contains undefined
