957145889a7f3baf7eeb04aedc3e0ba524b51b2b
kanchan7c <kanchan.chaudhary77@gmail.com>
2024-02-27 23:19:09 +0530

    added code to write diff datatypes and functions

--- datatypes.js
+++ datatypes.js
@@ -11,4 +11,37 @@ let state; // undefined
 // symbol => unique
 // object
 
-console.log(typeof cat); // output is object
+console.log(typeof cat); //output is object
+
+//Primitive datatypes (call by value)
+//7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId); //false
+
+// Reference type (Non Primitive)(Call by reference)
+// Array, Objects, Functions
+
+// Array
+const myArray = [23, 'me', 87.29, 'hello world'];
+// Object
+let myObject = {
+  name: 'abc',
+  age: 30,
+};
+// 3 ways to write function
+// 1st method
+function myfunc() {
+  console.log('Hello myfunc');
+}
+
+// 2nd method
+let anotherFunction = function () {
+  console.log('hello anotherFunction');
+};
+
+// 3rd method or arrow function
+const thirdFunc = () => {
+  console.log('hello thirdFunc');
+};
