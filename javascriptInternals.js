7758cd46f76075438302cf65fc877692fa350f3f
kanchan7c <kanchan.chaudhary77@gmail.com>
2024-03-01 21:35:18 +0530

    js internals

---
+++ javascriptInternals.js
@@ -0,0 +1,10 @@
+// Execution context
+
+// whenever code runs, there are 2 execution context created
+
+// ---> Global Execution context (GEC)
+// ---> Function Execution context (FEC)
+
+// Process =========>
+// CODE  ---> Memory Creation Phase
+//       ---> Execution Phase
