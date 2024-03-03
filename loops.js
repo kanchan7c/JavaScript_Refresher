1b0f599719a6efa657af31d6efe13b676c8a285d
kanchan7c <kanchan.chaudhary77@gmail.com>
2024-03-03 17:01:51 +0530

    basic loops

---
+++ loops.js
@@ -0,0 +1,39 @@
+// for loop
+
+for (let index = 0; index < 10; index++) {
+  // console.log(index + 1); // prints 1 to 10
+}
+
+// while loop
+let index = 5;
+while (index > 0) {
+  // console.log(index);
+  index--;
+}
+
+// do while loop
+do {
+  // console.log(index);
+  index--;
+} while (index > 0);
+
+// pattern printing using loops
+
+for (let line = 1; line <= 5; line++) {
+  // console.log('* '.repeat(line));
+}
+//  o/p
+// *
+// **
+// ***
+// ****
+// *****
+
+for (let i = 5; i >= 1; i--) {
+  // console.log('* '.repeat(i));
+}
+// *****
+// ****
+// ***
+// **
+// *
